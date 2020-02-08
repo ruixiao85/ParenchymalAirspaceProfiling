1b6f72a01765d49843f752bd4d8dc2196816bc05
ruixiao85 <ruixiao85@gmail.com>
2020-02-08 17:40:16 -0600

    denote research-use and release date. long term version.

--- App.xaml.cs
+++ App.xaml.cs
@@ -19,13 +19,7 @@ namespace LungMorphApp
 
 		public async void StartApp(object sender, StartupEventArgs e)
 		{
-            string ExpirationDate="20-06";
-            string UserName="Researcher";
-            bool EnableBatch=UtilityIO.ValidateInternetTimeNtp(ExpirationDate)
-                ||UtilityIO.ValidateInternetTimeHttp(ExpirationDate) ; //|| UtilityIO.ValidateSystemTime(ExpirationDate)
-
-            UserWindow user = new UserWindow(UserName,ExpirationDate,EnableBatch); user.Show();
-
+            UserWindow user = new UserWindow("Research-Use","20-02",true); user.Show();
 		}
 
 
